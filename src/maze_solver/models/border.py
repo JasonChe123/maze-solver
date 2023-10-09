5427437b0994c4a62137d8a42aee3873151670ed
Jason Che <kcche2001@yahoo.com.hk>
2023-10-09 21:58:41 +0100

    Define border.

--- src/maze_solver/models/border.py
+++ src/maze_solver/models/border.py
@@ -1,1 +1,33 @@
-# use 4 bits to represent each scenario of a square with/without wall(s)
+# Use 4 bits to represent each scenario of a square with/without wall(s)
+# Using IntFlag can do some bitwise operation
+# Using property decorator (without declare a setter method) to make it a read-only property
+
+from enum import IntFlag, auto
+
+
+class Border(IntFlag):
+    EMPTY = 0
+    TOP = auto()
+    BOTTOM = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+    @property
+    def corner(self) -> bool:
+        # bitwise operation
+        return self in (
+            self.TOP | self.LEFT,
+            self.TOP | self.RIGHT,
+            self.BOTTOM | self.LEFT,
+            self.BOTTOM | self.RIGHT,
+        )
+
+    @property
+    def dead_end(self) -> bool:
+        # if 3 bits (3 borders in a spare) which means a dead end
+        return self.bit_count() == 3
+
+    @property
+    def intersection(self) -> bool:
+        # if only 1 bit (1 border in a spare) which means a T (intersection)
+        return self.bit_count() < 2
